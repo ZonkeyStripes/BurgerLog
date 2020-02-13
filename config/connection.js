054379a9d37499572b3547bdbc4c0425e668c11d
Harley <Zonkeystripes@hotmail.com>
2020-02-12 21:52:06 -0700

    added jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,24 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "rootpass",
-    database: "burger_db"
-});
+if(process.env.JAWSDB_URL){
+    const connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "rootpass",
+        database: "burger_db"
+    });
+}
+
 
 connection.connect(function(error) {
     if (error) {
         console.error("Opps!! =>", error.stack);
         return;
     }
-    console.log("Connected as :", connection.threadID);
+    console.log("Connected as :", connection.port);
 });
 
 module.exports = connection;
